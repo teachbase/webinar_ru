6f8aea6bb9cb8619d22105d36d67c1b2b4b4bd8f
Alexander Shvaykin <skiline.alex@gmail.com>
2020-04-08 17:20:56 +0300

    add rubocop

--- lib/webinar_ru.rb
+++ lib/webinar_ru.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "webinar_ru/version"
 
 module WebinarRu
--- spec/webinar_ru_spec.rb
+++ spec/webinar_ru_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe WebinarRu do
   it "has a version number" do
     expect(WebinarRu::VERSION).not_to be nil
