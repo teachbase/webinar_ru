a14163bfc4e86c55350f95472888abe27b62233d
fargelus <ddraudred@gmail.com>
2022-12-22 19:50:12 +0400

    Simplest spec for proxy

--- spec/webinar_ru/api/connection_spec.rb
+++ spec/webinar_ru/api/connection_spec.rb
@@ -26,43 +26,28 @@ RSpec.describe WebinarRu::Api::Connection do
         "rack.logger" => nil
       }
     end
-
-    it "sends request" do
-      stub = stub_request(
+    let!(:stub) do
+      stub_request(
         :get, "https://test.webinar.ru/v3/eventsessions/some-uniq-id"
       ).with(
         headers: {
           "x-auth-token" => "test-token", "Content-Type" => "application/x-www-form-urlencoded"
         }
       ).to_return(body: "abc", status: 200, headers: { "test" => "123" })
+    end
 
+    it "sends request" do
       call_connection
       expect(stub).to have_been_requested
     end
 
     context "with proxy" do
-      before { WebMock.allow_net_connect! }
-      let(:conn) { described_class.new(proxy: proxy) }
+      let(:conn) { described_class.new(proxy: "https://teachbase.com:8888") }
 
-      context "when proxy address with port" do
-        let(:proxy) { "https://teachbase.com:8888" }
-
-        it "connects to proxy first" do
-          expect { call_connection }.to raise_error(SocketError,
-                                                    %r{ open TCP connection to #{proxy} })
-        end
+      it "sends request" do
+        call_connection
+        expect(stub).to have_been_requested
       end
-
-      context "when proxy as host" do
-        let(:proxy) { "https://teachbase.com" }
-
-        it "connects to proxy first" do
-          expect { call_connection }.to raise_error(SocketError,
-                                                    /open TCP connection to #{proxy}:443/)
-        end
-      end
-
-      after { WebMock.disable_net_connect! }
     end
   end
 end
